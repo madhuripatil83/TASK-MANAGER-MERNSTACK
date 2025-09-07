856a7fcf8122a52412ffc4ea84db57bc73631058
Madhuri <madhuripatilblore@gmail.com>
2025-09-07 18:58:07 +0530

    added newtask

--- frontend_task_manager/src/App.jsx
+++ frontend_task_manager/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 import { TaskManager } from "./components/TaskManager";
+import Test from "./components/Test";
 
 function App() {
   return (
@@ -13,6 +12,7 @@ function App() {
       </h1>
 
       <TaskManager />
+      <Test/>
     </div>
   );
 }
